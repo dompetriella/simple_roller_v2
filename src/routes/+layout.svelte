313b189c5253b30d00fc2f7814014c49364f0116
dompetriella <dompetriella@gmail.com>
2026-02-02 22:30:38 -0500

    portal set up, math time math time

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,26 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-	import '../app.css';
+  import CanvasPortalTarget from '$lib/components/CanvasPortalTarget.svelte'
+  import { Canvas } from '@threlte/core'
+  import type { Snippet } from 'svelte'
+  import '../app.css';
 
-	let { children } = $props();
+  let { children }: { children: Snippet } = $props()
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
+<div>
+  <Canvas>
+    <CanvasPortalTarget />
+  </Canvas>
+</div>
 
 {@render children()}
+
+<style>
+  div {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import CanvasPortal from "$lib/components/CanvasPortal.svelte";
+	import { T } from "@threlte/core";
+
+</script>
+<!-- Regular DOM elements for UI -->
+<button>Click me</button>
+
+<div class="three-container">
+<CanvasPortal>
+  <T.PerspectiveCamera
+    position.z={10}
+    makeDefault
+  />
+
+  <T.Mesh>
+    <T.BoxGeometry />
+    <T.MeshBasicMaterial color="blue" />
+  </T.Mesh>
+</CanvasPortal>
+</div>
+
+<style>
+.three-container {
+    height: 5em;
+    width: 5em;
+    background-color: aqua;
+}
+</style>
